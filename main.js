31b92dd577dac57b87ffa5f64b8e48e03cf263a1
sachinkumar28 <sachinkumar31102018@email.com>
2022-05-11 02:11:57 +1000

    first Project

--- main.js
+++ main.js
@@ -14,6 +14,12 @@ var nine = document.querySelector(".box9");
 var winningText = document.querySelector(".winningText");
 var turn = document.querySelector(".turn");
 turn.textContent = "X turn";
+var scoreone = document.querySelector(".score1");
+scoreone.textContent = 0;
+var scoreone_count = 0;
+var scoretwo = document.querySelector(".score2");
+scoretwo.textContent = 0;
+var scoretwo_count = 0;
 var count = 0;
 one.textContent = emptyString;
 two.textContent = emptyString;
@@ -34,11 +40,11 @@ function startGame(event) {
   ) {
     if (playerTurn == "X") {
       event.target.textContent = "X";
-      turn.textContent = "Now O turn";
+      turn.textContent = "O turn";
       playerTurn = "O";
     } else {
       event.target.textContent = "O";
-      turn.textContent = "Now X turn";
+      turn.textContent = "X turn";
       playerTurn = "X";
     }
   }
@@ -50,112 +56,160 @@ function startGame(event) {
     two.textContent == three.textContent
   ) {
     winningText.textContent = "X is the Winner!";
+    scoreone_count++;
+    scoreone.textContent = scoreone_count;
     winningCondition = true;
+    playerTurn = "X";
   } else if (
     one.textContent == "X" &&
     one.textContent == four.textContent &&
     four.textContent == seven.textContent
   ) {
     winningText.textContent = "X is the Winner!";
+    scoreone_count++;
+    scoreone.textContent = scoreone_count;
     winningCondition = true;
+    playerTurn = "X";
   } else if (
     one.textContent == "X" &&
     one.textContent == five.textContent &&
     five.textContent == nine.textContent
   ) {
     winningText.textContent = "X is the Winner!";
+    scoreone_count++;
+    scoreone.textContent = scoreone_count;
     winningCondition = true;
+    playerTurn = "X";
   } else if (
     two.textContent == "X" &&
     two.textContent == five.textContent &&
     five.textContent == eight.textContent
   ) {
     winningText.textContent = "X is the Winner!";
+    scoreone_count++;
+    scoreone.textContent = scoreone_count;
     winningCondition = true;
+    playerTurn = "X";
   } else if (
     four.textContent == "X" &&
     four.textContent == five.textContent &&
     five.textContent == six.textContent
   ) {
     winningText.textContent = "X is the Winner!";
+    scoreone_count++;
+    scoreone.textContent = scoreone_count;
     winningCondition = true;
+    playerTurn = "X";
   } else if (
     three.textContent == "X" &&
     three.textContent == six.textContent &&
     six.textContent == nine.textContent
   ) {
     winningText.textContent = "X is the Winner!";
+    scoreone_count++;
+    scoreone.textContent = scoreone_count;
     winningCondition = true;
+    playerTurn = "X";
   } else if (
     three.textContent == "X" &&
     three.textContent == five.textContent &&
     five.textContent == seven.textContent
   ) {
     winningText.textContent = "X is the Winner!";
+    scoreone_count++;
+    scoreone.textContent = scoreone_count;
     winningCondition = true;
+    playerTurn = "X";
   } else if (
     seven.textContent == "X" &&
     seven.textContent == eight.textContent &&
     eight.textContent == nine.textContent
   ) {
     winningText.textContent = "X is the Winner!";
+    scoreone_count++;
+    scoreone.textContent = scoreone_count;
     winningCondition = true;
+    playerTurn = "X";
   } else if (
     one.textContent == "O" &&
     one.textContent == two.textContent &&
     two.textContent == three.textContent
   ) {
     winningText.textContent = "O is the Winner!";
+    scoretwo_count++;
+    scoretwo.textContent = scoretwo_count;
     winningCondition = true;
+    playerTurn = "O";
   } else if (
     one.textContent == "O" &&
     one.textContent == four.textContent &&
     four.textContent == seven.textContent
   ) {
     winningText.textContent = "O is the Winner!";
+    scoretwo_count++;
+    scoretwo.textContent = scoretwo_count;
     winningCondition = true;
+    playerTurn = "O";
   } else if (
     one.textContent == "O" &&
     one.textContent == five.textContent &&
     five.textContent == nine.textContent
   ) {
     winningText.textContent = "O is the Winner!";
+    scoretwo_count++;
+    scoretwo.textContent = scoretwo_count;
     winningCondition = true;
+    playerTurn = "O";
   } else if (
     two.textContent == "O" &&
     two.textContent == five.textContent &&
     five.textContent == eight.textContent
   ) {
     winningText.textContent = "O is the Winner!";
+    scoretwo_count++;
+    scoretwo.textContent = scoretwo_count;
     winningCondition = true;
+    playerTurn = "O";
   } else if (
     four.textContent == "O" &&
     four.textContent == five.textContent &&
     five.textContent == six.textContent
   ) {
     winningText.textContent = "O is the Winner!";
+    scoretwo_count++;
+    scoretwo.textContent = scoretwo_count;
     winningCondition = true;
+    playerTurn = "O";
   } else if (
     three.textContent == "O" &&
     three.textContent == six.textContent &&
     six.textContent == nine.textContent
   ) {
     winningText.textContent = "O is the Winner!";
+    scoretwo_count++;
+    scoretwo.textContent = scoretwo_count;
     winningCondition = true;
+    playerTurn = "O";
   } else if (
     three.textContent == "O" &&
     three.textContent == five.textContent &&
     five.textContent == seven.textContent
   ) {
     winningText.textContent = "O is the Winner!";
+    scoretwo_count++;
+    scoretwo.textContent = scoretwo_count;
     winningCondition = true;
+    playerTurn = "O";
   } else if (
     seven.textContent == "O" &&
     seven.textContent == eight.textContent &&
     eight.textContent == nine.textContent
   ) {
     winningText.textContent = "O is the Winner!";
+    scoretwo_count++;
+    scoretwo.textContent = scoretwo_count;
     winningCondition = true;
+    playerTurn = "O";
   } else if (count == 9) {
     winningText.textContent = "It's a Draw!";
   }
@@ -175,4 +229,5 @@ resetButton.addEventListener("click", function (event1) {
   winningText.textContent = emptyString;
   count = 0;
   winningCondition = false;
+  playerTurn = "X";
 });
